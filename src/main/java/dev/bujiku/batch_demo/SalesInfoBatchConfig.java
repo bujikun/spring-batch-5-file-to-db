b6ceebbdc85595c3dea47ae8f4114962a630c6df
Newton Bujiku <bujikun@vodacom.co.tz>
2024-10-09 02:21:34 +0300

    Declare JPAItemWriter

--- src/main/java/dev/bujiku/batch_demo/SalesInfoBatchConfig.java
+++ src/main/java/dev/bujiku/batch_demo/SalesInfoBatchConfig.java
@@ -1,7 +1,11 @@
 package dev.bujiku.batch_demo;
 
+import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -9,8 +13,6 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
-import java.math.BigDecimal;
 
 /**
  * @author Newton Bujiku
@@ -20,10 +22,11 @@ import java.math.BigDecimal;
 @EnableBatchProcessing
 @RequiredArgsConstructor
 public class SalesInfoBatchConfig {
+    private final EntityManagerFactory entityManagerFactory;
 
     @Bean
-    public FlatFileItemReader<SalesInfo> itemReader(LineMapper<SalesInfo> lineMapper) {
-        var reader = new FlatFileItemReader<SalesInfo>();
+    public ItemReader<SalesInfoDTO> itemReader(LineMapper<SalesInfoDTO> lineMapper) {
+        var reader = new FlatFileItemReader<SalesInfoDTO>();
         reader.setResource(new ClassPathResource("data/sales-info.csv"));
         reader.setName("salesInfoItemReader");
         reader.setLineMapper(lineMapper);
@@ -32,16 +35,28 @@ public class SalesInfoBatchConfig {
     }
 
     @Bean
-    public LineMapper<SalesInfo> lineMapper() {
+    public LineMapper<SalesInfoDTO> lineMapper() {
 
         var tokenizer = new DelimitedLineTokenizer();
         tokenizer.setDelimiter(",");
         tokenizer.setNames("product", "seller", "sellerId", "price", "city", "category");
         tokenizer.setStrict(false);
 
-        var mapper = new DefaultLineMapper<SalesInfo>();
+        // var fieldSetMapper = new BeanWrapperFieldSetMapper<SalesInfoDTO>();
+        //fieldSetMapper.setTargetType(SalesInfoDTO.class);
+
+
+        var mapper = new DefaultLineMapper<SalesInfoDTO>();
         mapper.setLineTokenizer(tokenizer);
+        //mapper.setFieldSetMapper(fieldSetMapper);
         return mapper;
     }
+
+    public JpaItemWriter<SalesInfo> itemWriter() {
+        return new JpaItemWriterBuilder<SalesInfo>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
+
 
 }
