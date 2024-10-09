16204a6cf1a00e7a8bcf69df2864acdfd1d7cd32
Newton Bujiku <bujikun@vodacom.co.tz>
2024-10-09 16:35:50 +0300

    Enable scheduling

--- src/main/java/dev/bujiku/batch_demo/BatchDemoApplication.java
+++ src/main/java/dev/bujiku/batch_demo/BatchDemoApplication.java
@@ -2,12 +2,14 @@ package dev.bujiku.batch_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BatchDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BatchDemoApplication.class, args);
+    }
 
 }
