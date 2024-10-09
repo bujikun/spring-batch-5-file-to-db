5c3e7a5b7ed95bb2b32a852f28dee8afa1e83a47
Newton Bujiku <bujikun@vodacom.co.tz>
2024-10-09 12:45:44 +0300

    Add event listener to run the job when the app is ready

---
+++ src/main/java/dev/bujiku/batch_demo/SalesInfoJobRunner.java
@@ -0,0 +1,48 @@
+package dev.bujiku.batch_demo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import java.time.Instant;
+
+/**
+ * @author Newton Bujiku
+ * @since 2024
+ */
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class SalesInfoJobRunner {
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    @EventListener(classes = ApplicationReadyEvent.class)
+    void run() {
+        log.info("ABOUT TO START A JOB");
+        var jobParams = new JobParametersBuilder()
+                .addLong("startedAt", Instant.now().toEpochMilli())
+                .toJobParameters();
+        try {
+            jobLauncher.run(job, jobParams);
+        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException |
+                 JobParametersInvalidException e) {
+            log.error("", e);
+        }
+    }
+
+
+//    @Bean
+//    public CommandLineRunner commandLineRunner() {
+//
+//    }
+}
