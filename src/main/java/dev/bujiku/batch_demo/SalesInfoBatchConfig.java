1d4e872a25cae8cbc0d99b78f17c191a8ba9dedf
Newton Bujiku <bujikun@vodacom.co.tz>
2024-10-09 12:57:26 +0300

    Remove unused code

--- src/main/java/dev/bujiku/batch_demo/SalesInfoBatchConfig.java
+++ src/main/java/dev/bujiku/batch_demo/SalesInfoBatchConfig.java
@@ -31,47 +31,18 @@ public class SalesInfoBatchConfig {
 
     @Bean
     public ItemReader<SalesInfoDTO> itemReader() {
-//        var reader = new FlatFileItemReader<SalesInfoDTO>();
-//        reader.setResource(new ClassPathResource("data/sales-info.csv"));
-//        reader.setName("salesInfoItemReader");
-//        reader.setLineMapper(lineMapper());
-//        reader.setLinesToSkip(1);
-//        return reader;
         return new FlatFileItemReaderBuilder<SalesInfoDTO>()
                 .resource(new ClassPathResource("data/sales-info.csv"))
                 .name("salesInfoItemReader")
                 .targetType(SalesInfoDTO.class)
                 .strict(true)
-                //.lineMapper(lineMapper())
                 .linesToSkip(1)
                 .delimited()
                 .delimiter(",")
                 .strict(false)
                 .names("product", "seller", "sellerId", "price", "city", "category")
-                //.lineTokenizer(lineTokenizer())
                 .build();
     }
-
-//    public LineTokenizer lineTokenizer() {
-//        var tokenizer = new DelimitedLineTokenizer();
-//        tokenizer.setDelimiter(",");
-//        tokenizer.setNames("product", "seller", "sellerId", "price", "city", "category");
-//        tokenizer.setStrict(false);
-//        return tokenizer;
-//    }
-
-    //@Bean
-//    public LineMapper<SalesInfoDTO> lineMapper() {
-//
-//
-//        var fieldSetMapper = new RecordFieldSetMapper<>(SalesInfoDTO.class);
-//
-//        var mapper = new DefaultLineMapper<SalesInfoDTO>();
-//        // mapper.setLineTokenizer(tokenizer);
-//        mapper.setFieldSetMapper(fieldSetMapper);
-//
-//        return mapper;
-//    }
 
     @Bean
     public RepositoryItemWriter<SalesInfo> itemWriter() {
@@ -80,7 +51,6 @@ public class SalesInfoBatchConfig {
         return writer;
     }
 
-    //@Bean
     public TaskExecutor taskExecutor() {
         var executor = new SimpleAsyncTaskExecutor();
         executor.setConcurrencyLimit(100);
